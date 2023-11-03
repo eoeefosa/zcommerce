7cb077e52a072cd5ca521066ce3e9c44ac6a7701
eoeefosa <eoeefosa@gmail.com>
2023-11-03 21:36:09 +0100

    started cartegory screen

--- lib/view/categories.dart
+++ lib/view/categories.dart
@@ -5,10 +5,37 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body: Center(
-        child: Text("Categories"),
-      ),
-    );
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Row(
+          children: [
+            SizedBox(
+              width: size.width * .2,
+              child: const Column(
+                children: [
+                  ListTile(title: Text("Grocery")),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: size.width * .8,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  children: [
+                    Card(
+                      color: Colors.black,
+                      child: SizedBox(
+                        height: 50,
+                        width: size.width * .8,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
